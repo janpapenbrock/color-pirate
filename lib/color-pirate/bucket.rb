d4d30a58ae339f8d302aa622bc8142bc39a1a244
janpapenbrock <lunoxx@gmail.com>
2013-09-27 11:59:57 +0200

    Basic bucket implementation.

--- lib/color-pirate/bucket.rb
+++ lib/color-pirate/bucket.rb
@@ -1,2 +1,18 @@
 class Bucket
+
+	attr_reader :params
+
+	def initialize params={}
+		@params = params
+	end
+
+	def objects
+		@objects ||= []
+	end
+
+	def add object
+		objects << object
+		objects.uniq!
+	end
+
 end
--- spec/color-pirate/bucket_spec.rb
+++ spec/color-pirate/bucket_spec.rb
@@ -2,9 +2,14 @@ require "spec_helper"
 
 describe Bucket do
 	
+	before :each do
+		@bucket = Bucket.new
+		@object = double("object")
+	end
+
 	describe "#new" do
 		it "should accept no arguments" do
-			lambda { Bucket.new }.should be_instance_of Bucket
+			Bucket.new.should be_instance_of Bucket
 		end
 
 		it "should accept one argument" do
@@ -12,5 +17,29 @@ describe Bucket do
 		end
     end
 
-	
+    describe "#objects" do
+    	it "should contain no objects first" do
+    		@bucket.objects.count.should eql 0
+    	end
+
+    	it "should have a list of objects added to the bucket" do
+    		@bucket.add(@object)
+    		@bucket.objects.count.should eql 1
+    	end
+	end
+
+	describe "#add" do
+		it "should add an object to the list of objects" do
+			count = @bucket.objects.count
+			@bucket.add @object
+			@bucket.objects.count.should eql count + 1
+		end
+
+		it "should add an object only once" do
+			count = @bucket.objects.count
+			@bucket.add @object
+			@bucket.add @object
+			@bucket.objects.count.should eql count + 1
+		end
+	end
 end
